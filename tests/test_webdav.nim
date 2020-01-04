77745f5d7705d07b1d6a5414fc96b237af1c0477
Beshr Kayali <me@beshr.com>
2020-01-04 12:46:23 +0100

    Tests: Assert exception get raised on fails

--- tests/test_webdav.nim
+++ tests/test_webdav.nim
@@ -10,17 +10,43 @@ when isMainModule:
   # Make directory
   waitFor wd.mkdir(path = "files/")
 
+  # This will fail because directory already exists
+  doAssertRaises(OperationFailed):
+    waitFor wd.mkdir(path = "files/")
+
   # Upload
   waitFor wd.upload(
     filepath = "tests/example.md",
     destination = "files/example.md"
   )
 
+  # File not found
+  doAssertRaises(OperationFailed):
+    waitFor wd.upload(
+      filepath = "tests/does_not_exist.md",
+      destination = "files/example.md"
+    )
+
+  # File already exists
+  doAssertRaises(OperationFailed):
+    waitFor wd.upload(
+      filepath = "tests/example.md",
+      destination = "files/example.md"
+    )
+
   # Move
   waitFor wd.mv(path = "files/example.md", destination = "example.md")
 
+  # Moving again will fail because it already exists
+  doAssertRaises(OperationFailed):
+    waitFor wd.mv(path = "files/example.md", destination = "example.md")
+
   # Copy
   waitFor wd.cp(path = "example.md", destination = "files/example.md")
+
+  # Already exists
+  doAssertRaises(OperationFailed):
+    waitFor wd.cp(path = "example.md", destination = "files/example.md")
 
   # List
   let t = waitFor wd.ls(
@@ -42,3 +68,7 @@ when isMainModule:
   waitFor wd.rm("example.md")
   waitFor wd.rm("files/example.md")
   waitFor wd.rm("files/")
+
+  # Does not exist
+  doAssertRaises(OperationFailed):
+    waitFor wd.rm("example.md")
